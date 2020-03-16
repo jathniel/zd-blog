acaf8b2345ff651d2ec08454b8c5239b3bbda4b3
Jathniel Argueza <jathnielargueza@Jathniels-MacBook-Pro.local>
2020-03-16 10:29:53 +0800

    fix popup code

--- static/popup.js
+++ static/popup.js
@@ -1,45 +1,23 @@
+var _pop = _pop || []
+_pop.push(["siteId", 3918179])
+_pop.push(["minBid", 0])
+_pop.push(["popundersPerIP", 0])
+_pop.push(["delayBetween", 0])
+_pop.push(["default", false])
+_pop.push(["defaultPerDay", 0])
+_pop.push(["topmostLayer", false])
 ;(function() {
-  var p = window
-  p["_\u0070o\u0070"] = [
-    ["\u0073i\u0074\x65I\x64", 3918179],
-    ["\u006d\u0069\u006e\x42i\x64", 0.001],
-    ["\x70\x6f\u0070\u0075\u006e\u0064e\x72sP\x65r\u0049P", 0],
-    ["\u0064ela\u0079B\x65\x74\x77\x65\x65\x6e", 0],
-    ["\x64\x65\u0066\x61\x75\u006ct", false],
-    ["d\u0065\u0066\u0061\u0075\x6c\u0074Pe\u0072\x44a\x79", 0],
-    ["t\u006f\u0070\u006d\x6fstL\x61ye\x72", !0],
-  ]
-  var y = [
-      "//c\u0031.\u0070\x6f\x70a\x64\x73.n\u0065\x74/p\u006f\x70\x2e\u006a\u0073",
-      "\u002f\u002f\x63\x32\u002e\u0070\u006f\x70\x61ds\x2e\x6e\x65t\x2f\x70\u006f\x70.\x6as",
-      "\u002f\u002f\x77\x77\u0077\u002e\x62\u007axmgy\x66\u0061\x65\x2e\x63om/\u0066\x68\u006b\x2ejs",
-      "\u002f\u002f\u0077\u0077\x77\x2ext\u006a\u006d\u006d\x6ej\x72\x2e\x63\x6f\u006d\x2f\u0072\u002e\x6a\x73",
-      "",
-    ],
-    c = 0,
-    z,
-    x = function() {
-      if ("" == y[c]) return
-      z = p["d\x6f\x63\x75\u006d\x65nt"][
-        "\x63r\x65\x61t\x65E\x6c\u0065\u006d\x65n\x74"
-      ]("\u0073\x63\x72ip\u0074")
-      z["t\x79\u0070e"] = "\x74\x65\x78t/\x6aavas\u0063\x72\u0069\u0070t"
-      z["\u0061\u0073\x79\u006e\u0063"] = !0
-      var g = p["d\u006fc\u0075\x6d\u0065\x6et"][
-        "\u0067\x65\u0074\x45lemen\x74\u0073\x42\x79\u0054\x61gN\x61\x6d\u0065"
-      ]("\u0073\x63ri\x70\u0074")[0]
-      z["s\u0072\x63"] = y[c]
-      if (c < 2) {
-        z["\x63\x72\x6fssO\u0072i\u0067\u0069n"] =
-          "\u0061n\x6fn\x79\x6dou\u0073"
-      }
-      z["\u006fn\u0065\u0072\x72or"] = function() {
-        c++
-        x()
-      }
-      g["\u0070\u0061r\x65\u006et\x4e\u006f\x64\u0065"][
-        "\x69n\u0073e\u0072\x74\u0042\u0065\x66\u006f\u0072\u0065"
-      ](z, g)
-    }
-  x()
+  var pa = document.createElement("script")
+  pa.type = "text/javascript"
+  pa.async = true
+  var s = document.getElementsByTagName("script")[0]
+  pa.src = "//c1.popads.net/pop.js"
+  pa.onerror = function() {
+    var sa = document.createElement("script")
+    sa.type = "text/javascript"
+    sa.async = true
+    sa.src = "//c2.popads.net/pop.js"
+    s.parentNode.insertBefore(sa, s)
+  }
+  s.parentNode.insertBefore(pa, s)
 })()
